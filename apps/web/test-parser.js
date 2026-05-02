d33b323fb31b8184e938d392399d15cceaf5002f
syedmukheeth <smpeer05@gmail.com>
2026-05-02 10:50:40 +0530

    chore(cleanup): remove remaining test-parser script

--- apps/web/test-parser.js
+++
@@ -1,8 +0,0 @@
-import { parseErrors } from "../../apps/web/src/services/errorParser.js";
-
-const gccOutput = `main.cpp: In function 'int main()':
-main.cpp:5:3: error: expected primary-expression before 'return'
-    5 |   return 0;
-      |   ^~~~~~`;
-
-console.log(JSON.stringify(parseErrors(gccOutput, "cpp"), null, 2));
